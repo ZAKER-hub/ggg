b85e94dc9405e7b40c842282947e744862b6b1a1
Denis <51993467+ZAKER-hub@users.noreply.github.com>
2021-06-21 07:43:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const authRouter =require('./routes/authRouter')
+const exec = require('child_process').execFile;
+const { count } = require('console');
+
+const main = express()
+main.use(express.json())
+main.use("/auth", authRouter)
+
+const db_path = "./data/db"
+const db_port = 27017
+const server_port = 5000
+
+const start = async () => {
+    
+    try{
+        await exec("mongod.exe", ['--dbpath', db_path, '--port', db_port],  function(err, data) {
+            console.log(err)
+            console.log(data.toString());
+        })
+        console.log(`MongoDB запущена на порту ${db_port}`);
+        await mongoose.connect(`mongodb://localhost:${db_port}/db`, { useUnifiedTopology: true, useNewUrlParser: true })
+            .then(() => console.log('MongoDB подключена'))
+            .catch(error => console.log(error))
+        main.listen(server_port, () => console.log(`Сервер запущен на порту ${server_port}`))
+    }
+    catch (e){
+        console.log(e)
+    }
+}
+
+start()
